2612e6cca1d2b8f68eda4e7f1e78e7a2e10394bc
krishnakumarbaditya <109644191+krishnakumarbaditya@users.noreply.github.com>
2022-07-26 01:15:09 +0530

    Update Iterative_Control_Statements.c

--- Iterative_Control_Statements.c
+++ Iterative_Control_Statements.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int main(){
-    // 1.Write a program to print MySirG N times on the screen.
+    // 1.Write a program to print MySirG 5 times on the screen.
     int i=1;
     while(i<=5)
     {
@@ -8,7 +8,7 @@ int main(){
         i++;
     }
     printf("\n");
-    // 2.Write a program to print the first N natural numbers.
+    // 2.Write a program to print the first 10 natural numbers.
     int no=1;
     printf("First 10 natuaral number. \n");
     while(no<=10)
@@ -17,7 +17,7 @@ int main(){
         no++;
     }
     printf("\n");
-    // 3.Write a program to print the first N natural numbers in reverse order.
+    // 3.Write a program to print the first 10 natural numbers in reverse order.
     int no1=10;
     printf("First 10 natural numbers in reverse order.\n");
     while(no1>=1){
@@ -25,7 +25,7 @@ int main(){
         no1--;
     }
     printf("\n");
-    // 4.Write a program to print the first N odd natural numbers
+    // 4.Write a program to print the first 10 odd natural numbers
     int no2=1;
     printf("First 10 odd natural numbers.\n");
     while(no2<=20){
@@ -34,7 +34,7 @@ int main(){
         no2++;
     }
     printf("\n");
-    //5.Write a program to print the first N odd natural numbers in reverse order.
+    //5.Write a program to print the first 10 odd natural numbers in reverse order.
     int no3=20;
     printf("First 10 odd natural numbers in reverse order.\n");
     while(no3>=1){
@@ -43,7 +43,7 @@ int main(){
         no3--;
     }
     printf("\n");
-    // 6.Write a program to print the first N even natural numbers.
+    // 6.Write a program to print the first 10 even natural numbers.
     int no4=1;
     printf("First 10 even natural numbers.\n");
     while(no4<=20){
@@ -52,7 +52,7 @@ int main(){
         no4++;
     }
     printf("\n");
-    // 7.Write a program to print the first N even natural numbers in reverse order.
+    // 7.Write a program to print the first 10 even natural numbers in reverse order.
     int no5=20;
     printf("First 10 even natural numbers in reverse order.\n");
     while(no5>=1){
@@ -61,7 +61,7 @@ int main(){
         no5--;
     }
     printf("\n");
-    // 8.Write a program to print squares of the first N natural numbers.
+    // 8.Write a program to print squares of the first 10 natural numbers.
     int no6,sq;
     printf("Square of first 10 natural numbers.\n");
     for(no6=1;no6<=10;no6++){
@@ -69,7 +69,7 @@ int main(){
         printf("%d * % d = %d\n",no6,no6,sq);
     }
     printf("\n");
-    // 9.Write a program to print cubes of the first N natural numbers.
+    // 9.Write a program to print cubes of the first 10 natural numbers.
     int no7,cu;
     printf("Cube of first 10 natural numbers.\n");
     for(no7=1;no7<=10;no7++){
@@ -77,7 +77,7 @@ int main(){
         printf("%d * %d * %d = %d\n",no7,no7,no7,cu);
     }
     printf("\n");
-    // 10.Write a program to print a table of N.
+    // 10.Write a program to print a table of 5.
     int no8,tb,no9=5;
     printf("Print a table of 5\n");
     for(no8=1;no8<=10;no8++){
