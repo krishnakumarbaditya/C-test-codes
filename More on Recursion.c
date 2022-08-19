fb5a25cd72a45dec3363ec2a06ddef920c121993
krishnakumarbaditya <109644191+krishnakumarbaditya@users.noreply.github.com>
2022-08-19 18:03:14 +0530

    Update More on Recursion.c

--- More on Recursion.c
+++ More on Recursion.c
@@ -66,3 +66,70 @@ int evennatural(int no1)
         return sum;
     }
 }
+
+//4. Write a recursive function to calculate sum of squares of first n natural numbers
+#include<stdio.h>
+int square(int);
+
+int main()
+{
+    int no,sum1;
+    printf("calculate sum of squares of first n natural numbers ");
+    scanf("%d",&no);
+    sum1=square(no);
+    printf("Sum of the squares of first %d natural numbers is %d.",no,sum1);
+    return 0;
+}
+int square(int no1)
+{
+    int sum;
+    if(no1)
+    {
+        sum=(no1*no1)+square(no1-1);
+        return sum;
+    }
+}
+
+//5. Write a recursive function to calculate sum of digits of a given number
+#include<stdio.h>
+int digits(int);
+
+int main()
+{
+    int no,sum1;
+    printf("calculate sum of digits of a given number ");
+    scanf("%d",&no);
+    sum1=digits(no);
+    printf("Sum of the digits of %d is %d.",no,sum1);
+    return 0;
+}
+int digits(int no1)
+{
+    int sum;
+    if(no1)
+    {
+        sum=digits(no1/10)+(no1%10);
+        return sum;
+    }
+}
+
+//6. Write a recursive function to calculate factorial of a given number
+#include<stdio.h>
+int fact(int);
+
+int main()
+{
+    int no,fact1;
+    printf("calculate factorial of a given number ");
+    scanf("%d",&no);
+    fact1=fact(no);
+    printf("factorial of %d is %d.",no,fact1);
+    return 0;
+}
+int fact(int no1)
+{
+    int fact1;
+    if(no1==0 || no1==1)
+        return 1;
+    fact1=no1*fact(no1-1);
+}
