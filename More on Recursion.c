d5b8e37587dd0b2c38e731d89b193397cb8fdbd5
krishnakumarbaditya <109644191+krishnakumarbaditya@users.noreply.github.com>
2022-08-19 17:24:36 +0530

    Update More on Recursion.c

--- More on Recursion.c
+++ More on Recursion.c
@@ -20,3 +20,49 @@ int natural(int no1)
         return sum;
     }
 }
+
+//2. Write a recursive function to calculate sum of first N odd natural numbers
+#include<stdio.h>
+int oddnatural(int);
+
+int main()
+{
+    int no,sum1;
+    printf("calculate sum of first N odd natural numbers ");
+    scanf("%d",&no);
+    sum1=oddnatural(no);
+    printf("Sum of the first %d odd natural number is %d.",no,sum1);
+    return 0;
+}
+int oddnatural(int no1)
+{
+    int sum;
+    if(no1)
+    {
+        sum=(no1*2-1)+oddnatural(no1-1);
+        return sum;
+    }
+}
+
+//3. Write a recursive function to calculate sum of first N even natural numbers
+#include<stdio.h>
+int evennatural(int);
+
+int main()
+{
+    int no,sum1;
+    printf("calculate sum of first N even natural numbers ");
+    scanf("%d",&no);
+    sum1=evennatural(no);
+    printf("Sum of the first %d even natural number is %d.",no,sum1);
+    return 0;
+}
+int evennatural(int no1)
+{
+    int sum;
+    if(no1)
+    {
+        sum=(no1*2)+evennatural(no1-1);
+        return sum;
+    }
+}
