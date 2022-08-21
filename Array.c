5abfac2dc73d0c0b786d9bfba101ec69a1dd740c
krishnakumarbaditya <109644191+krishnakumarbaditya@users.noreply.github.com>
2022-08-21 20:20:42 +0530

    Update Array.c

--- Array.c
+++ Array.c
@@ -136,6 +136,73 @@ int main()
     return 0;
 }
 
+/*
+7. Write a program to find second largest in an array.Take array values from the user.
+*/
+#include<stdio.h>
+int main()
+{
+    int i,j,arr[7];
+    int L,S_L;
+    printf("Enter the elements in array.\n");
+    for(i=0;i<7;i++)
+        scanf("%d",&arr[i]);
+    printf("Elements of array\n");
+    for(i=0;i<7;i++)
+        printf("%d\t",arr[i]);
+    L=arr[0];
+    for(i=0;i<7;i++)
+    {
+        if(arr[i+1]>L)
+        {
+            S_L=L;
+            L=arr[i+1];
+        }
+        else if(arr[i+1]>S_L)
+        {
+            S_L=arr[i+1];
+        }
+    }
+    printf("\nFirst Largest no. is %d\n",L);
+    printf("Second Largest no. is %d\n",S_L);
+
+    return 0;
+}
+
+/*
+8. Write a program to find the second smallest number in an array.Take array values from the user.
+*/
+#include<stdio.h>
+int main()
+{
+    int i,j,arr[7];
+    int S,S_S;
+    printf("Enter the elements in array.\n");
+    for(i=0;i<7;i++)
+        scanf("%d",&arr[i]);
+    printf("Elements of array\n");
+    for(i=0;i<7;i++)
+        printf("%d\t",arr[i]);
+    S=9999;
+    S_S=9999;
+    for(i=0;i<7;i++)
+    {
+        if(arr[i]<S)
+        {
+            S_S=S;
+            S=arr[i];
+        }
+
+        if(arr[i]>S && arr[i]<S_S)
+        {
+            S_S = arr[i];
+        }
+    }
+    printf("\nFirst Smallest no. is %d\n",S);
+    printf("Second Smallest no. is %d\n",S_S);
+
+    return 0;
+}
 
 
 
