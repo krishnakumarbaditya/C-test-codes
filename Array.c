45fe03ffeb189339ef1c7d718ba1398818d18c9b
krishnakumarbaditya <109644191+krishnakumarbaditya@users.noreply.github.com>
2022-08-21 12:21:14 +0530

    Update Array.c

--- Array.c
+++ Array.c
@@ -60,3 +60,104 @@ int main()
     printf("sum of odd number is %d.",od);
     return 0;
 }
+
+/*
+4. Write a program to find the greatest number stored in an array of size 10. Take array
+values from the user.
+*/
+#include<stdio.h>
+int main()
+{
+    int i,arr[10];
+    printf("Enter the elements ");
+    for(i=0;i<10;i++)
+        scanf("%d",&arr[i]);
+    int max=arr[0];
+    for(i=0;i<10;i++)
+    {
+        if(arr[i]>max)
+            max=arr[i];
+    }
+    printf("Greatest number among the array is %d",max);
+    return 0;
+}
+
+/*
+5. Write a program to find the smallest number stored in an array of size 10. Take array
+values from the user.
+*/
+#include<stdio.h>
+int main()
+{
+    int i,arr[10];
+    printf("Enter the elements ");
+    for(i=0;i<10;i++)
+        scanf("%d",&arr[i]);
+    int min=arr[0];
+    for(i=0;i<10;i++)
+    {
+        if(arr[i]<min)
+            min=arr[i];
+    }
+    printf("Smallest number among the array is %d",min);
+    return 0;
+}
+
+/*
+6. Write a program to sort elements of an array of size 10. Take array values from the
+user.
+*/
+#include<stdio.h>
+int main()
+{
+    int i,j,arr[10];
+    printf("Enter the elements ");
+    for(i=0;i<10;i++)
+        scanf("%d",&arr[i]);
+    printf("Number before sorting.\n");
+    for(i=0;i<10;i++)
+        printf("%d\t",arr[i]);
+    for(i=0;i<10;i++)
+    {
+        int temp=0;
+        for(j=i+1;j<10;j++)
+        {
+            if(arr[i]>arr[j])
+            {
+                temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }
+        }
+    }
+    printf("\nNumber after sorting.\n");
+    for(i=0;i<10;i++)
+        printf("%d\t",arr[i]);
+    return 0;
+}
+
+
+
+
+/*
+10. Write a program in C to copy the elements of one array into another array.Take array values from the user
+*/
+#include<stdio.h>
+int main()
+{
+    int i,j,arr[10];
+    int arr1[10];
+    printf("Enter the elements in array1.\n");
+    for(i=0;i<10;i++)
+        scanf("%d",&arr[i]);
+    printf("Elements of array1\n");
+    for(i=0;i<10;i++)
+        printf("%d\t",arr[i]);
+    for(i=0;i<10;i++)
+        arr1[i]=arr[i];
+    printf("\nElements of array2\n");
+    for(i=0;i<10;i++)
+        printf("%d\t",arr1[i]);
+
+    return 0;
+}
