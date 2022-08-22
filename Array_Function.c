7247d72d095f2f0966d938713005cd1203c036cd
krishnakumarbaditya <109644191+krishnakumarbaditya@users.noreply.github.com>
2022-08-22 22:46:39 +0530

    Update Array_Function.c

--- Array_Function.c
+++ Array_Function.c
@@ -5,6 +5,7 @@ int main()
 {
     int a[5];
     printf("%d is greatest number among the elements",greatest(a,5));
+    return 0;
 }
 int greatest(int b[],int n)
 {
@@ -21,4 +22,94 @@ int greatest(int b[],int n)
         }
     }
     return max;
+}
+
+//2. Write a function to find the smallest number from the given array of any size. (TSRS)
+#include<stdio.h>
+int smallest(int[],int);
+int main()
+{
+    int a[5];
+    printf("%d is smallest number among the elements",smallest(a,5));
+    return 0;
+}
+int smallest(int b[],int n)
+{
+    int i;
+    printf("Enter the numbers of element.");
+    for(i=0;i<n;i++)
+        scanf("%d",&b[i]);
+    int min=b[0];
+    for(i=0;i<n;i++)
+    {
+        if(b[i]<=min)
+        {
+            min=b[i];
+        }
+    }
+    return min;
+}
+
+//3. Write a function to sort an array of any size. (TSRS)
+#include<stdio.h>
+void sort(int[],int);
+int main()
+{
+    int a[5];
+    sort(a,5);
+    return 0;
+}
+void sort(int b[],int n)
+{
+    int i,j;
+    printf("Enter the numbers of element.");
+    for(i=0;i<n;i++)
+        scanf("%d",&b[i]);
+    printf("\nNumber before sorting.\n");
+    for(i=0;i<n;i++)
+        printf("%d\t",b[i]);
+    int temp=0;
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(b[j]<b[i])
+            {
+                temp=b[j];
+                b[j]=b[i];
+                b[i]=temp;
+            }
+        }
+    }
+    printf("\nNumber after sorting.\n");
+    for(i=0;i<n;i++)
+        printf("%d\t",b[i]);
+}
+
+/*
+6. Write a function in C to read n number of values in an array and display it in reverse
+order.
+*/
+#include<stdio.h>
+void reverse(int[],int);
+int main()
+{
+    int a[5];
+    reverse(a,5);
+    return 0;
+}
+void reverse(int b[],int n)
+{
+    int i;
+    printf("Enter the elements in array.\n");
+    for(i=0;i<n;i++)
+        scanf("%d",&b[i]);
+    for(i=0;i<n;i++)
+        printf("%d\t",b[i]);
+
+    printf("\nPrint Elements of array in reverse order.\n");
+    for(i=n-1;i>=0;i--)
+        printf("%d\t",b[i]);
+
+    return 0;
 }
