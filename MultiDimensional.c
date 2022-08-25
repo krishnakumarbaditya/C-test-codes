625755472be25238437cb8ce24cff2b6d26e194c
krishnakumarbaditya <109644191+krishnakumarbaditya@users.noreply.github.com>
2022-08-25 12:36:45 +0530

    Update MultiDimensional.c

--- MultiDimensional.c
+++ MultiDimensional.c
@@ -120,4 +120,272 @@ int main()
         }
         printf("\n");
     }
+    return 0;
+}
+
+//3. Write a program in C to find the transpose of a given matrix.
+
+#include<stdio.h>
+int main()
+{
+    int a[3][3],b[3][3];
+    int i,j;
+    printf("Enter the number of elements in matrices.\n");
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    printf("Before transpose of given matrix.\n");
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            printf("%d\t",a[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("After transpose of given matrix.\n");
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            b[i][j]=a[j][i];
+        }
+    }
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            printf("%d\t",b[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
+//4. Write a program in C to find the sum of right diagonals of a matrix.
+
+#include<stdio.h>
+int main()
+{
+    int a[3][3],b[3][3];
+    int i,j,sum=0;
+    printf("Enter the number of elements in matrices.\n");
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    printf("Number of elements in given matrix.\n");
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            printf("%d\t",a[i][j]);
+        }
+        printf("\n");
+    }
+
+
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            if(i==j)
+            {
+                sum=sum+a[i][j];
+            }
+        }
+    }
+    printf("sum of right diagonals of given matrix is %d.\n",sum);
+    return 0;
+}
+
+//5. Write a program in C to find the sum of left diagonals of a matrix.
+
+#include<stdio.h>
+int main()
+{
+    int a[3][3],b[3][3];
+    int i,j,sum=0;
+    printf("Enter the number of elements in matrices.\n");
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    printf("Number of elements in given matrix.\n");
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            printf("%d\t",a[i][j]);
+        }
+        printf("\n");
+    }
+
+    int m=3;
+    for(i=0;i<3;i++)
+    {
+        m=m-1;
+        for(j=0;j<3;j++)
+        {
+            if(j==m)
+            {
+                sum=sum+a[i][j];
+            }
+        }
+    }
+    printf("sum of left diagonals of given matrix is %d.\n",sum);
+    return 0;
+}
+
+//6. Write a program in C to find the sum of rows and columns of a Matrix.
+
+#include<stdio.h>
+int main()
+{
+    int a[3][3],b[3][3];
+    int i,j,rsum=0,csum=0;
+    printf("Enter the number of elements in matrices.\n");
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    printf("Number of elements in given matrix.\n");
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            printf("%d\t",a[i][j]);
+        }
+        printf("\n");
+    }
+
+
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            rsum=rsum+a[i][j];
+        }
+        printf("%d row sum is %d.\n",i+1,rsum);
+        rsum=0;
+    }
+    printf("\n");
+    for(j=0;j<3;j++)
+    {
+        for(i=0;i<3;i++)
+        {
+            csum=csum+a[i][j];
+        }
+        printf("%d column sum is %d.\n",j+1,csum);
+        csum=0;
+    }
+    return 0;
+}
+
+//7. Write a program in C to print or display the lower triangular of a given matrix.
+
+#include<stdio.h>
+int main()
+{
+    int a[3][3],b[3][3];
+    int i,j,rsum=0,csum=0;
+    printf("Enter the number of elements in matrices.\n");
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    printf("Number of elements in given matrix.\n");
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            printf("%d\t",a[i][j]);
+        }
+        printf("\n");
+    }
+
+
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            if(i<j)
+                a[i][j]=0;
+        }
+    }
+    printf("Display the lower triangular of a given matrix.\n");
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            printf("%d\t",a[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
+//8. Write a program in C to print or display the upper triangular of a given matrix.
+
+#include<stdio.h>
+int main()
+{
+    int a[3][3],b[3][3];
+    int i,j,rsum=0,csum=0;
+    printf("Enter the number of elements in matrices.\n");
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    printf("Number of elements in given matrix.\n");
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            printf("%d\t",a[i][j]);
+        }
+        printf("\n");
+    }
+
+
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            if(i>j)
+                a[i][j]=0;
+        }
+    }
+    printf("Display the upper triangular of a given matrix.\n");
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            printf("%d\t",a[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
 }
