9391a3e0fe0e95246795ce1f7b3b9c7b6f9c2e80
krishnakumarbaditya <109644191+krishnakumarbaditya@users.noreply.github.com>
2022-08-25 10:51:19 +0530

    Update MultiDimensional.c

--- MultiDimensional.c
+++ MultiDimensional.c
@@ -56,3 +56,62 @@ int main()
         printf("\n");
     }
 }
+
+//2. Write a program to calculate the product of two matrices each of order 3x3.
+
+#include<stdio.h>
+int main()
+{
+    int a[3][3],b[3][3],c[3][3];
+    int i,j;
+    printf("Enter the number first matrices.\n");
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            printf("%d\t",a[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("Enter the number second matrices.\n");
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            scanf("%d",&b[i][j]);
+        }
+    }
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            printf("%d\t",b[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("Product of two matrices are : \n");
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            c[i][j]=a[i][j]*b[i][j];
+        }
+    }
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            printf("%d\t",c[i][j]);
+        }
+        printf("\n");
+    }
+}
