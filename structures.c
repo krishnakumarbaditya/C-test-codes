7e2ec7b079f12f2306385b12f835c4626a7017d8
krishnakumarbaditya <109644191+krishnakumarbaditya@users.noreply.github.com>
2022-09-24 18:57:16 +0530

    Add files via upload

---
+++ structures.c
@@ -0,0 +1,11 @@
+//1. Define a structure Employee with member variables id, name, salary
+#include<stdio.h>
+int main()
+{
+    struct Employee
+    {
+        int id
+        char name[20];
+        double salary;
+    };
+}
