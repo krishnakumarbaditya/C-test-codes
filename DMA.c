0a13c0e515f9f4c9b891cfe0169b05fc19f5b98f
krishnakumarbaditya <109644191+krishnakumarbaditya@users.noreply.github.com>
2022-09-27 19:04:44 +0530

    Update DMA.c

--- DMA.c
+++ DMA.c
@@ -165,3 +165,138 @@ int main()
 
     free(ptr);
 }
+/*
+6. Write a program in C to find the largest element using Dynamic Memory Allocation.
+*/
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int *ptr,c;
+
+    int i=0, size=0,j=0;
+    printf("Enter the size : ");
+    scanf("%d",&size);
+
+    ptr = (int*)malloc(sizeof(int));
+
+    if(ptr==NULL)
+    {
+        printf("Memory Allocation is failed");
+        return 0;
+    }
+    printf("The entered %d values \n",size);
+
+    for(i=0;i<size;i++)
+    {
+        printf("Enter element %3d: ",i+1);
+        scanf("%d",&c);
+        j++;
+
+        ptr = (int*)realloc(ptr,j*sizeof(int));
+        ptr[i]=c;
+    }
+    int max=0;
+    for(i=0;i<size;i++)
+    {
+        if(ptr[i]>max)
+        {
+            max=ptr[i];
+        }
+    }
+    printf("\nLargest no is %d",max);
+
+    free(ptr);
+}
+/*
+7. Write a program to demonstrate memory leak in C.
+*/
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int *ptr;
+    ptr = (int*)malloc(sizeof(int));
+    ptr = NULL;
+    return 0;
+}
+/*
+8. Write a program to demonstrate dangling pointers in C.
+*/
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int *ptr;
+    ptr = (int*)malloc(sizeof(int));
+    *ptr = 10;
+    printf("Before free %d\n",*ptr);
+    free(ptr);
+    printf("After free %d",*ptr);
+
+    return 0;
+}
+/*
+9. Write a program to allocate memory dynamically of the size in bytes entered by the
+user. Also handle the case when memory allocation is failed.
+*/
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int *ptr;
+    ptr = (int*)malloc(sizeof(int));
+    *ptr = 10;
+    if(ptr==NULL)
+    {
+        printf("Memory Allocation is failed");
+        return 0;
+    }
+}
+//10. Find out the maximum and minimum from an array using dynamic memory allocation in C
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int *ptr,c;
+
+    int i=0, size=0,j=0;
+    printf("Enter the size : ");
+    scanf("%d",&size);
+
+    ptr = (int*)malloc(sizeof(int));
+
+    if(ptr==NULL)
+    {
+        printf("Memory Allocation is failed");
+        return 0;
+    }
+    printf("The entered %d values \n",size);
+
+    for(i=0;i<size;i++)
+    {
+        printf("Enter element %3d: ",i+1);
+        scanf("%d",&c);
+        j++;
+
+        ptr = (int*)realloc(ptr,j*sizeof(int));
+        ptr[i]=c;
+    }
+    int max=0;
+    int min=99999;
+    for(i=0;i<size;i++)
+    {
+        if(ptr[i]>max)
+        {
+            max=ptr[i];
+        }
+        if(ptr[i]<min)
+        {
+            min=ptr[i];
+        }
+    }
+    printf("Maximum no is %d\n",max);
+    printf("Minimum no is %d",min);
+
+    free(ptr);
+}
